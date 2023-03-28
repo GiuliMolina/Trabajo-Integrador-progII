a5a78b840948de739d05803856041ffdf440f815
Nicole Witomski <nwitomski@udesa.edu.ar>
2023-03-28 16:06:35 -0300

    cataolog

---
+++ trabajo-integrador/db/index.js
@@ -0,0 +1,18 @@
+catalogo = [
+    {
+        marca: 'Nike',
+        modelo: 'Air Force',
+        cantidadEnStock: 5,
+        color: 'blanco',
+        id: 1,
+        imagen: ,
+    },
+    {
+        marca: ,
+        modelo: ,
+        cantidadEnStock: ,
+        color: ,
+        id: ,
+        imagen: ,
+    },
+]
