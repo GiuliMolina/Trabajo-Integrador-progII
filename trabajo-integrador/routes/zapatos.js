71cc21fd652150437ccf9cc09d571ac38c84faf9
Desmondrattagan <drattagan@udesa.edu.ar>
2023-03-28 16:20:44 -0300

    controladores

--- trabajo-integrador/routes/zapatos.js
+++ trabajo-integrador/routes/zapatos.js
@@ -1,5 +1,18 @@
 const zapatos = require('../db/index')
-
 const express = require('express')
 const router = express.Router()
 
+router.get('/', zapatosController.index)
+
+router.get('/:id', zapatosController.details)
+
+router.get('/marca/:marca', zapatosController.porMarca)
+
+router.get('/color/:color', zapatosController.porColor)
+
+router.get('/talle/:talle', zapatosController.porTalle)
+
+router.get('/sexo/:sexo', zapatosController.porSexo)
+
+
+module.exports = router
