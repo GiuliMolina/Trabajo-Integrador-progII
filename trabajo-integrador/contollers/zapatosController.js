4454f500d3bb655a8529c8fc2268419c98e3289f
Nicole Witomski <nwitomski@udesa.edu.ar>
2023-03-28 16:31:58 -0300

    cambio

---
+++ trabajo-integrador/contollers/zapatosController.js
@@ -0,0 +1,26 @@
+const zapatos = require('../db/index')
+
+const controlador = {
+    index : function(req, res){
+        res.send(zapatos)
+    },
+    // details: function(req,res){
+    //     res.send()
+    // },
+    // porMarca: function(req,res){
+    //     res.send()
+    // },
+    // porColor: function(req,res){
+    //     res.send()
+    // },
+    // porTalle: function(req,res){
+    //     res.send()
+    // },
+    // porSexo: function(req,res){
+    //     res.send()
+    // }
+}
+
+module.exports = controlador
+
+
--- trabajo-integrador/routes/zapatos.js
+++ trabajo-integrador/routes/zapatos.js
@@ -1,18 +1,18 @@
-const zapatos = require('../db/index')
 const express = require('express')
 const router = express.Router()
+const zapatosController = require ('../contollers/zapatosController')
 
 router.get('/', zapatosController.index)
 
-router.get('/:id', zapatosController.details)
+// router.get('/:id', zapatosController.details)
 
-router.get('/marca/:marca', zapatosController.porMarca)
+// router.get('/marca/:marca', zapatosController.porMarca)
 
-router.get('/color/:color', zapatosController.porColor)
+// router.get('/color/:color', zapatosController.porColor)
 
-router.get('/talle/:talle', zapatosController.porTalle)
+// router.get('/talle/:talle', zapatosController.porTalle)
 
-router.get('/sexo/:sexo', zapatosController.porSexo)
+// router.get('/sexo/:sexo', zapatosController.porSexo)
 
 
 module.exports = router
