af4e9df20073efbffa087479c654e57eb0a11ce3
Desmondrattagan <drattagan@udesa.edu.ar>
2023-03-29 17:32:01 -0300

    Data

--- trabajo-integrador/data/index.js
+++ trabajo-integrador/data/index.js
@@ -1,25 +1,4 @@
-catalogo = [
-    {
-        marca: 'Nike',
-        modelo: 'Air Force',
-        talle: 40,
-        cantidadEnStock: 5,
-        color: 'blanco',
-        sexo: "masculino",
-        id: 1,
-        imagen: 'img-cafetera-moulinex.jpg',
-    },
-    {
-        marca: 'Nike',
-        modelo: 'Jordan',
-        talle: 36,
-        cantidadEnStock: 6,
-        color: 'negro',
-        sexo: 'femenino',
-        id: 2,
-        imagen: 'img-home-banner.jpg',
-    },
-]
+
 
 data = [
    { usuario:{
