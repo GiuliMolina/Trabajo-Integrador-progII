9b4b8d8ab5e66ba792e8e8533e480632d9780e60
Desmondrattagan <drattagan@udesa.edu.ar>
2023-03-29 17:29:50 -0300

    Data

---
+++ trabajo-integrador/data/index.js
@@ -0,0 +1,77 @@
+
+data = [
+   { usuario:{
+      nombre: 'Joaco',
+      email: 'GiuliLaIndia@gmail.com',
+      imagen: 'img'
+   },
+
+    productos:[
+      0 ={
+      imagen: 'hola',
+      nombre: 'hola',
+      descripcion: 'hola' ,
+      fecha: 'hola',
+
+    }, 1= {
+      imagen: 'hola',
+      nombre: 'hola',
+      descripcion: 'hola' ,
+      fecha: 'hola',
+    }, 2={
+      imagen: 'hola',
+      nombre: 'hola',
+      descripcion: 'hola' ,
+      fecha: 'hola',
+    }, 3= {
+      imagen: 'hola',
+      nombre: 'hola',
+      descripcion: 'hola' ,
+      fecha: 'hola',
+    },  4= {
+      imagen: 'hola',
+      nombre: 'hola',
+      descripcion: 'hola' ,
+      fecha: 'hola'
+    },  5= {
+      imagen: 'hola',
+      nombre: 'hola',
+      descripcion: 'hola' ,
+      fecha: 'hola'
+    },  6= {
+      imagen: 'hola',
+      nombre: 'hola',
+      descripcion: 'hola' ,
+      fecha: 'hola'
+    },  7= {
+      imagen: 'hola',
+      nombre: 'hola',
+      descripcion: 'hola' ,
+      fecha: 'hola'
+    },  8= {
+      imagen: 'hola',
+      nombre: 'hola',
+      descripcion: 'hola' ,
+      fecha: 'hola'
+    },  9= {
+      imagen: 'hola',
+      nombre: 'hola',
+      descripcion: 'hola' ,
+      fecha: 'hola'
+    }
+    ],
+
+    comentarios:{
+      nombre: 'hola',
+      texto: 'hola',
+      imagen: 'hola'
+    }
+
+
+     
+   },
+  
+]
+
+
+module.exports = data
