eeb3db9e38d7e5ea4ae80fe0172126f33f7ae753
Nicole Witomski <nwitomski@udesa.edu.ar>
2023-04-04 17:21:42 -0300

    cambio

--- trabajo-integrador/routes/zapatos.js
+++ trabajo-integrador/routes/zapatos.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const zapatosController = require ('../contollers/zapatosController')
 
-
+router.get('/', zapatosController.controladorIndex.index)
 router.get('/:id', zapatosController.controladorProducts.products)
 
 
