76132b3abff4f0868d98c837e3a8c60f701730e0
Nicole Witomski <nwitomski@udesa.edu.ar>
2023-04-05 17:35:20 -0300

    data

--- trabajo-integrador/data/index.js
+++ trabajo-integrador/data/index.js
@@ -1,16 +1,25 @@
-let data =
-{
+let data = {
   usuario:{
       nombre: 'Joaco',
       email: 'GiuliLaIndia@gmail.com',
       imagen: 'img'
    },
 
-    productos:[
+  productos:[
     {
       imagen: './images/amarillo.webp',
       nombre: 'Topper amarillas',
       descripcion: 'hola' ,
+      comentarios:[ {
+        nombre: 'Daniel',
+        texto: 'hola',
+        imagen: './images/users/default-image'
+      },
+      {
+        nombre: 'Daniel',
+        texto: 'hola',
+        imagen: './images/users/default-image'
+      }],
       fecha: 'fecha1',
       id:0
 
@@ -18,48 +27,128 @@ let data =
       imagen: './images/blancas.webp',
       nombre: 'Botitas blancas',
       descripcion: 'hola' ,
+      comentarios:[ {
+        nombre: 'Daniel',
+        texto: 'hola',
+        imagen: './images/users/default-image'
+      },
+      {
+        nombre: 'Daniel',
+        texto: 'hola',
+        imagen: './images/users/default-image'
+      }],
       fecha: 'fecha2',
       id:1
     },{
       imagen: './images/celeste.webp',
       nombre: 'Zapatillas celeste',
       descripcion: 'hola' ,
+      comentarios:[ {
+        nombre: 'Daniel',
+        texto: 'hola',
+        imagen: './images/users/default-image'
+      },
+      {
+        nombre: 'Daniel',
+        texto: 'hola',
+        imagen: './images/users/default-image'
+      }],
       fecha: 'fecha3',
       id:2
     },{
       imagen: './images/color.webp',
       nombre: 'Nike botitas coloridas',
       descripcion: 'hola' ,
+      comentarios:[ {
+        nombre: 'Daniel',
+        texto: 'hola',
+        imagen: './images/users/default-image'
+      },
+      {
+        nombre: 'Daniel',
+        texto: 'hola',
+        imagen: './images/users/default-image'
+      }],
       fecha: 'fecha4',
       id:3
     },{
       imagen: './images/converse_negra.webp',
       nombre: 'Converse All Star negras',
       descripcion: 'hola' ,
+      comentarios:[ {
+        nombre: 'Daniel',
+        texto: 'hola',
+        imagen: './images/users/default-image'
+      },
+      {
+        nombre: 'Daniel',
+        texto: 'hola',
+        imagen: './images/users/default-image'
+      }],
       fecha: 'fecha5',
       id:4
     },{
       imagen: './images/lila.webp',
       nombre: 'Zapatillas 47 Street',
       descripcion: 'hola' ,
+      comentarios:[ {
+        nombre: 'Daniel',
+        texto: 'hola',
+        imagen: './images/users/default-image'
+      },
+      {
+        nombre: 'Daniel',
+        texto: 'hola',
+        imagen: './images/users/default-image'
+      }],
       fecha: 'fecha6',
       id:5
     },{
       imagen: './images/nike_black.webp',
       nombre: 'Nike Air Force negras',
       descripcion: 'hola' ,
+      comentarios:[ {
+        nombre: 'Daniel',
+        texto: 'hola',
+        imagen: './images/users/default-image'
+      },
+      {
+        nombre: 'Daniel',
+        texto: 'hola',
+        imagen: './images/users/default-image'
+      }],
       fecha: 'fecha7',
       id:6
     },{
       imagen: './images/nike_gris.webp',
       nombre: 'Nike Air Force grises',
       descripcion: 'hola' ,
+      comentarios:[ {
+        nombre: 'Daniel',
+        texto: 'hola',
+        imagen: './images/users/default-image'
+      },
+      {
+        nombre: 'Daniel',
+        texto: 'hola',
+        imagen: './images/users/default-image'
+      }],
       fecha: 'fecha8',
       id:7
     },{
       imagen: './images/nike_negras.webp',
       nombre: 'Nike Air Force negras y blanco',
       descripcion: 'hola' ,
+      comentarios:[ {
+        nombre: 'Daniel',
+        texto: 'hola',
+        imagen: './images/users/default-image'
+      },
+      {
+        nombre: 'Daniel',
+        texto: 'hola',
+        imagen: './images/users/default-image'
+      }],
       fecha: 'fecha9',
       id:8
     }
@@ -67,17 +156,21 @@ let data =
       imagen: './images/vans_botita.jpeg',
       nombre: 'Vans negras',
       descripcion: 'hola' ,
+      comentarios:[ {
+        nombre: 'Daniel',
+        texto: 'hola',
+        imagen: './images/users/default-image'
+      },
+      {
+        nombre: 'Daniel',
+        texto: 'hola',
+        imagen: './images/users/default-image'
+      }],
       fecha: 'fecha10',
       id:9
     }
-    ],
+    ]
 
-    comentarios:[ {
-      nombre: 'Daniel',
-      texto: 'hola',
-      imagen: './images/users/default-image'
-    },
-  ]
   }
 
 module.exports = data
