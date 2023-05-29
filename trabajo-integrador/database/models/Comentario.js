78226ec687464745765e4372442425ef442a0826
Desmondrattagan <drattagan@udesa.edu.ar>
2023-05-29 16:33:55 -0300

    modelo_comentarios

---
+++ trabajo-integrador/database/models/Comentario.js
@@ -0,0 +1,47 @@
+module.exports = function(sequelize,DataTypes){
+    let alias= Comentario;
+    let cols={
+        id:{
+            primaryKey: true,
+            autoIncrement:true,
+            allowNull:false,
+            unsigned: true,
+            type: DataTypes.INTEGER
+        },
+        id_post:{
+           unsigned: true,
+           type: DataTypes.INTEGER,
+           allowNull:false
+
+        },
+        usuario_id:{
+            unsigned: true,
+            type: DataTypes.INTEGER,
+            allowNull:false
+        },
+        comentario:{
+            type:DataTypes.STRING,
+            allowNull: False
+        },
+        created_at:{
+            type: DataTypes.DATE
+        },
+        updated_at:{
+            type: DataTypes.DATE
+        },
+        deleted_at:{
+            type: DataTypes.DATE
+        }
+
+    };
+
+    let config= {
+        tableName: 'Comentario',
+        timestamps: true,
+        underscored: true
+    };
+
+    let Comentarios = sequelize.define(alias,cols,config);
+        return Comentarios
+}
+
