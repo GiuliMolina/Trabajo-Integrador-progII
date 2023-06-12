56c37b91c8dac5d8b4c33063917329646d384a76
Desmondrattagan <drattagan@udesa.edu.ar>
2023-06-12 17:09:43 -0300

    logout advances

--- trabajo-integrador/routes/index.js
+++ trabajo-integrador/routes/index.js
@@ -4,6 +4,7 @@ const indexController = require ('../controllers/indexController')
 
 
 router.get('/', indexController.index)
+router.post('/', indexcontroller.logout);
 
 module.exports = router;
  
