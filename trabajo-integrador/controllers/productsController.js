c1b1e6b1e8cf4b1d52d26882c2f0396896b0bf97
Nicole Witomski <nwitomski@udesa.edu.ar>
2023-06-12 09:17:29 -0300

    cambio

--- trabajo-integrador/controllers/productsController.js
+++ trabajo-integrador/controllers/productsController.js
@@ -53,8 +53,8 @@ const controladorProducts = {
                 imagen: imagen,
                 nombre_producto: nombre,
                 descripcion: text,
-                created_at: date,
-                usuario_id:10
+                // created_at: date,
+                // usuario_id:10
             })
             .then(function(data){
                 res.redirect('/')
